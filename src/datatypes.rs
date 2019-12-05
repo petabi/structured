c18f71f77d2e99f1ae06256a0761aac86947daf3
Min Kim <msk@dolbo.net>
2019-12-04 18:33:17 -0800

    Add schema serialization/deserialization (#16)
    * Add metadata to Schema
    
    * Serialize/deserialize Schema
    
    * Test serialization for all types

--- src/datatypes.rs
+++ src/datatypes.rs
@@ -1,10 +1,15 @@
-use serde::{Deserialize, Serialize};
+use serde::{
+    de::{Error, MapAccess, Visitor},
+    ser::SerializeMap,
+    Deserialize, Deserializer, Serialize, Serializer,
+};
 use serde_json::Value;
+use std::collections::HashMap;
 use std::fmt;
 use std::str::FromStr;
 
 /// Supported types.
-#[derive(Clone, Debug, PartialEq, Deserialize, Serialize, Copy)]
+#[derive(Clone, Debug, PartialEq, Copy)]
 pub enum DataType {
     Int64,
     Float64,
@@ -14,8 +19,115 @@ pub enum DataType {
     Utf8,
 }
 
-#[derive(Clone, Debug, Copy)]
+struct DataTypeVisitor;
+
+impl<'de> Visitor<'de> for DataTypeVisitor {
+    type Value = DataType;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a map descrbing a logical type")
+    }
+
+    fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+    where
+        A: MapAccess<'de>,
+    {
+        struct TypeProps<'a> {
+            name: Option<&'a str>,
+            is_signed: Option<bool>,
+            bit_width: Option<usize>,
+        }
+
+        let mut props = TypeProps {
+            name: None,
+            is_signed: None,
+            bit_width: None,
+        };
+        while let Some(k) = map.next_key()? {
+            match k {
+                "name" => props.name = map.next_value()?,
+                "isSigned" => props.is_signed = map.next_value()?,
+                "bitWidth" => props.bit_width = map.next_value()?,
+                _ => {}
+            }
+        }
+
+        match props.name {
+            Some("utf8") => Ok(DataType::Utf8),
+            Some("floatingpoint") => Ok(DataType::Float64),
+            Some("int") => match props.is_signed {
+                Some(true) => Ok(DataType::Int64),
+                Some(false) => Ok(DataType::Enum),
+                None => Err(A::Error::custom("isSigned missing or invalid")),
+            },
+            Some("timestamp") => Ok(DataType::DateTime),
+            Some("ipaddr") => Ok(DataType::IpAddr),
+            Some(name) => Err(A::Error::custom(format!("unknown type name: {}", name))),
+            None => Err(A::Error::custom("no type name")),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for DataType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_map(DataTypeVisitor)
+    }
+}
+
+impl Serialize for DataType {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            Self::Float64 => {
+                let mut map = serializer.serialize_map(Some(2))?;
+                map.serialize_entry("name", "floatingpoint")?;
+                map.serialize_entry("precision", "DOUBLE")?;
+                map.end()
+            }
+            Self::Int64 => {
+                let mut map = serializer.serialize_map(Some(3))?;
+                map.serialize_entry("name", "int")?;
+                map.serialize_entry("bitWidth", &64)?;
+                map.serialize_entry("isSigned", &true)?;
+                map.end()
+            }
+            Self::Utf8 => {
+                let mut map = serializer.serialize_map(Some(1))?;
+                map.serialize_entry("name", "utf8")?;
+                map.end()
+            }
+            Self::DateTime => {
+                let mut map = serializer.serialize_map(Some(2))?;
+                map.serialize_entry("name", "timestamp")?;
+                map.serialize_entry("unit", "SECOND")?;
+                map.end()
+            }
+            Self::IpAddr => {
+                let mut map = serializer.serialize_map(Some(3))?;
+                map.serialize_entry("name", "ipv4")?;
+                map.serialize_entry("bitWidth", &32)?;
+                map.serialize_entry("isSigned", &false)?;
+                map.end()
+            }
+            Self::Enum => {
+                let mut map = serializer.serialize_map(Some(3))?;
+                map.serialize_entry("name", "int")?;
+                map.serialize_entry("bitWidth", &32)?;
+                map.serialize_entry("isSigned", &false)?;
+                map.end()
+            }
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, Serialize)]
 pub struct Field {
+    #[serde(rename = "type")]
     data_type: DataType,
 }
 
@@ -50,17 +162,81 @@ impl Field {
     }
 }
 
-#[derive(Clone, Debug)]
+/// Describes the meta-data of an ordered sequence of relative types.
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Schema {
     fields: Vec<Field>,
+    /// A map of key-value pairs containing additional meta data.
+    #[serde(default)]
+    metadata: HashMap<String, String>,
 }
 
 impl Schema {
+    /// Creates a new `Schema` from a sequence of `Field` values
     pub fn new(fields: Vec<Field>) -> Self {
-        Self { fields }
+        Self::with_metadata(fields, HashMap::new())
+    }
+
+    /// Creates a new `Schema` from a sequence of `Field` values
+    /// and adds additional metadata in form of key value pairs.
+    pub fn with_metadata(fields: Vec<Field>, metadata: HashMap<String, String>) -> Self {
+        Self { fields, metadata }
     }
 
     pub fn fields(&self) -> &[Field] {
         &self.fields
+    }
+
+    /// Returns an immutable reference to the Map of custom metadata key-value pairs.
+    pub fn metadata(&self) -> &HashMap<String, String> {
+        &self.metadata
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn schema_to_json() {
+        let metadata: HashMap<String, String> = [("Key".to_string(), "Value".to_string())]
+            .iter()
+            .cloned()
+            .collect();
+        let schema = Schema::with_metadata(vec![Field::new(DataType::Utf8)], metadata);
+        assert_eq!(
+            serde_json::to_string(&schema).unwrap(),
+            r#"{"fields":[{"type":{"name":"utf8"}}],"metadata":{"Key":"Value"}}"#
+        );
+
+        let schema = Schema::new(vec![Field::new(DataType::Int64)]);
+        assert_eq!(
+            serde_json::to_string(&schema).unwrap(),
+            r#"{"fields":[{"type":{"name":"int","bitWidth":64,"isSigned":true}}],"metadata":{}}"#
+        );
+
+        let schema = Schema::new(vec![Field::new(DataType::Float64)]);
+        assert_eq!(
+            serde_json::to_string(&schema).unwrap(),
+            r#"{"fields":[{"type":{"name":"floatingpoint","precision":"DOUBLE"}}],"metadata":{}}"#
+        );
+
+        let schema = Schema::new(vec![Field::new(DataType::DateTime)]);
+        assert_eq!(
+            serde_json::to_string(&schema).unwrap(),
+            r#"{"fields":[{"type":{"name":"timestamp","unit":"SECOND"}}],"metadata":{}}"#
+        );
+
+        let schema = Schema::new(vec![Field::new(DataType::IpAddr)]);
+        assert_eq!(
+            serde_json::to_string(&schema).unwrap(),
+            r#"{"fields":[{"type":{"name":"ipv4","bitWidth":32,"isSigned":false}}],"metadata":{}}"#
+        );
+
+        let schema = Schema::new(vec![Field::new(DataType::Enum)]);
+        assert_eq!(
+            serde_json::to_string(&schema).unwrap(),
+            r#"{"fields":[{"type":{"name":"int","bitWidth":32,"isSigned":false}}],"metadata":{}}"#
+        );
     }
 }
