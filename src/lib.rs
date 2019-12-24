edc4214a9482280fe332d9bd8322436bad9f1e5f
Min Shao <mshao@petabi.com>
2019-12-23 17:56:37 -0800

    allow pub access to *ArrayType

--- src/lib.rs
+++ src/lib.rs
@@ -9,4 +9,7 @@ pub(crate) mod util;
 
 pub use datatypes::{DataType, Field, Schema, TimeUnit};
 pub use parse::records_to_columns;
-pub use table::{Column, ColumnType, Description, DescriptionElement, Table};
+pub use table::{
+    BinaryArrayType, Column, ColumnType, Description, DescriptionElement, Float64ArrayType,
+    Int32ArrayType, Int64ArrayType, Table, UInt32ArrayType, UInt8ArrayType, Utf8ArrayType,
+};
