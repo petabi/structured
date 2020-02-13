18d5890207f91915b4773f66aacdcbe8904ba3fc
Min Kim <msk@dolbo.net>
2020-02-13 12:46:28 -0800

    Remove an unused function (#50)

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ mod bitmap;
 pub mod csv;
 mod datatypes;
 mod memory;
-mod parse;
 pub mod record;
 mod table;
 pub(crate) mod util;
@@ -13,7 +12,6 @@ pub use datatypes::{
     DataType, Field, Float64Type, Int32Type, Int64Type, Schema, TimeUnit, TimestampSecondType,
     UInt32Type, UInt8Type,
 };
-pub use parse::records_to_columns;
 pub use table::{
     BinaryArrayType, Column, ColumnType, Description, DescriptionElement, Float64ArrayType,
     Int32ArrayType, Int64ArrayType, Table, UInt32ArrayType, UInt8ArrayType, Utf8ArrayType,
