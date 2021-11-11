3584a9adf0c5229983df44ff24deea6df6a4c620
syncpark <dkpark@petabi.com>
2021-11-11 16:32:49 +0900

    remove column types

--- src/table.rs
+++ src/table.rs
@@ -148,23 +148,25 @@ impl Table {
     }
 
     /// Return columns content for the detected event to send review
-    // TODO: consider `column_types`. MERGE this function to column_messages()
+    // * TODO: consider `column_types`.
+    // * TODO: MERGE this function to column_messages()
     #[must_use]
     pub fn column_raw_content(
         &self,
-        row_events: &[(usize, u64)],
-        _column_types: &Arc<Vec<ColumnType>>,
+        events: &[u64],
         target_columns: &[usize],
     ) -> HashMap<u64, Vec<Option<String>>> {
         let mut rst: HashMap<u64, Vec<Option<String>>> = HashMap::new();
         for column_id in target_columns {
             if let Some(column) = self.columns.get(*column_id) {
-                for (row_index, event_id) in row_events {
-                    let t = rst.entry(*event_id).or_insert_with(Vec::new);
-                    if let Ok(Some(message)) = column.string_try_get(*row_index) {
-                        t.push(Some(message.to_string()));
-                    } else {
-                        t.push(None);
+                for eventid in events {
+                    if let Some(row_index) = Self::event_index(&self, *eventid) {
+                        let t = rst.entry(*eventid).or_insert_with(Vec::new);
+                        if let Ok(Some(message)) = column.string_try_get(*row_index) {
+                            t.push(Some(message.to_string()));
+                        } else {
+                            t.push(None);
+                        }
                     }
                 }
             }
